c4df93c7a2f5d303206d441fb108b7322067596f
Shashank Manjunath <shashankmanjunath14@gmail.com>
2022-12-21 08:44:27 -0500

    Updated autocomplete to set up within the lsp_config file

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -1,3 +1,5 @@
+-- This file sets up the LSP for several languages, and autocompletion using those langauges as well
+
 -- Setting up LSP
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -33,6 +35,9 @@ require("mason-lspconfig").setup({
 	--       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
 	automatic_installation = false,
 })
+
+-- Set up nvim-cmp.
+local cmp = require("cmp")
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -70,6 +75,68 @@ local on_attach = function(client, bufnr)
 	end, bufopts)
 end
 
+-- Autocompletion setup
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
+
+-- Set up lspconfig autocomplete capability
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lsp_flags = {
 	-- This is the default in Nvim 0.7+
 	debounce_text_changes = 150,
@@ -78,45 +145,47 @@ local lsp_flags = {
 require("lspconfig")["pyright"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
-	settings = {
-		python = {
-			pythonPath = "$(which python)",
-		},
-		pyright = {},
-	},
+	capabilities = capabilities,
 })
 
 require("lspconfig")["texlab"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["clangd"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["cmake"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["sumneko_lua"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["bashls"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["dockerls"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
 
 require("lspconfig")["eslint"].setup({
 	on_attach = on_attach,
 	flags = lsp_flags,
+	capabilities = capabilities,
 })
