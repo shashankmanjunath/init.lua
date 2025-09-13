f2c4e406386b0c4cb89fea794d274c66c4c1248b
Shashank Manjunath <shashankmanjunath14@gmail.com>
2025-09-12 22:26:30 -0400

    Cleanup after/plugin section

--- after/plugin/lualine.lua
+++
@@ -1,5 +0,0 @@
-require("lualine").setup({
-	options = { theme = "ayu_mirage" },
-})
---
--- vim.api.nvim_set_var("webedvicons_enable", 1)
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -39,5 +39,84 @@ return {
 				{ name = "path" },
 			}),
 		})
+		-- Use an on_attach function to only map the following keys
+		-- after the language server attaches to the current buffer
+		local on_attach = function(client, bufnr)
+			-- Enable completion triggered by <c-x><c-o>
+			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+			-- Mappings.
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+			vim.keymap.set("n", "<space>wl", function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, bufopts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+			vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+			vim.keymap.set("n", "<space>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, bufopts)
+		end
+
+		-- Set up lspconfig.
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		--
+		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+		require("lspconfig")["pyright"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["texlab"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["clangd"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["cmake"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["lua_ls"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["bashls"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["dockerls"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
+
+		require("lspconfig")["eslint"].setup({
+			on_attach = on_attach,
+			-- flags = lsp_flags,
+			capabilities = capabilities,
+		})
 	end,
 }
