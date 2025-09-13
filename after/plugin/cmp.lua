5ba04173cc03b2629795b575f1fb01abf2422429
Shashank Manjunath <shashankmanjunath14@gmail.com>
2025-09-12 22:14:50 -0400

    Fixed autocompletion

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,123 +1,79 @@
-local cmp = require("cmp")
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-cmp.setup({
-	snippet = {
-		-- REQUIRED - you must specify a snippet engine
-		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
-			-- For `mini.snippets` users:
-			-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
-			-- insert({ body = args.body }) -- Insert at cursor
-			-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
-			-- require("cmp.config").set_onetime({ sources = {} })
-		end,
-	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		-- { name = "vsnip" }, -- For vsnip users.
-		{ name = "luasnip" }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-		{ name = "buffer" },
-	}),
-})
-
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]]
---
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
-	matching = { disallow_symbol_nonprefix_matching = false },
-})
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
+end
 
 -- Set up lspconfig.
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 --
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require("lspconfig")["pyright"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["texlab"].setup({
-	-- on_attach = on_attach,
-	flags = lsp_flags,
+	on_attach = on_attach,
+	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["clangd"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["cmake"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["lua_ls"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["bashls"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["dockerls"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
 
 require("lspconfig")["eslint"].setup({
-	-- on_attach = on_attach,
+	on_attach = on_attach,
 	-- flags = lsp_flags,
 	capabilities = capabilities,
 })
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,14 +2,42 @@ return {
 	"hrsh7th/nvim-cmp",
 	tag = "v0.0.2",
 	dependencies = {
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
+		{ "neovim/nvim-lspconfig" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
 
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
+		{
+			"L3MON4D3/LuaSnip",
+			tag = "v2.4.0",
+		},
+		{ "saadparwaiz1/cmp_luasnip" },
 	},
 	event = { "InsertEnter" },
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
 }
