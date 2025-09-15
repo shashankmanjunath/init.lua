04117b5088db373bc303cf99c33169d884eb8dde
Shashank Manjunath <shashankmanjunath14@gmail.com>
2025-09-15 10:43:17 -0400

    Changed autocomplete mappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -26,9 +26,9 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-r>"] = cmp.mapping.complete({}),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
 			}),
