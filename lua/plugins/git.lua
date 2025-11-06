f91d227c0bec556b2ad438463742845ada552555
Shashank Manjunath <smanjunath@anduril.com>
2025-11-06 15:57:26 -0500

    highlighting fix and new git maps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,7 +8,9 @@ return {
 		{ "<leader>gp", "<cmd>Git push<CR>" },
 		{ "<leader>gm", "<cmd>Git mergetool<CR>" },
 		{ "<leader>gd", "<cmd>Git difftool<CR>" },
-		{ "<leader>gv", "<cmd>Gvdiffsplit<CR>" },
-		{ "<leader>gg", "<cmd>diffget<CR>" },
+		{ "<leader>gv", "<cmd>Gvdiffsplit!<CR>" },
+		-- { "<leader>gg", "<cmd>diffget<CR>" },
+		{ "<leader>g2", "<cmd>diffget //2<CR>" },
+		{ "<leader>g3", "<cmd>diffget //3<CR>" },
 	},
 }
