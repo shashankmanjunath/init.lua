8dee4e6ad24c14ae275b3ef894b5e197e0bf078b
smanjunath <smanjunath@anduril.com>
2026-05-15 14:20:37 +0000

    Fixes to linter/formatter system

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -1,47 +1,46 @@
 return {
-	-- Linter/Formatter setup
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("lint").linters_by_ft = {
-				tex = { "lacheck" },
-				-- markdown = { "markdownlint" },
-				lua = { "luacheck" },
-				sh = { "shellcheck" },
-				dockerfile = { "hadolint" },
-				py = { "ruff" },
-				json = { "prettier" },
-				markdown = { "prettier" },
-			}
+  -- Linter/Formatter setup
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("lint").linters_by_ft = {
+        tex = { "lacheck" },
+        markdown = { "markdownlint" },
+        lua = { "luacheck" },
+        sh = { "shellcheck" },
+        dockerfile = { "hadolint" },
+        py = { "ruff" },
+        toml = { "taplo" },
+      }
 
-			-- Runs linter after write
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					python = { "ruff_format" },
-					markdown = { "prettier" },
-					telekasten = { "prettier" },
-					sh = { "shfmt" },
-					json = { "prettier" },
-				},
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
+      -- Runs linter after write
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "ruff_format" },
+          markdown = { "prettier" },
+          telekasten = { "prettier" },
+          sh = { "shfmt" },
+          json = { "prettier" },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,52 +1,54 @@
 return {
-	-- LSP Setup
-	{
-		"mason-org/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = function()
-			return {
-				-- A list of servers to automatically install if they're not already installed.
-				ensure_installed = {
-					-- Python
-					"pyright",
-					"ruff",
-					-- Latex
-					"texlab",
-					-- Markdown
-					"marksman",
-					-- C/C++
-					"clangd",
-					-- Cmake
-					"cmake",
-					-- Lua
-					"lua_ls",
-					-- Bash
-					"bashls",
-					-- Docker
-					"dockerls",
-					-- Javascript
-					"eslint",
-				},
-				-- Automatically install configured servers
-				automatic_installation = true,
-			}
-		end,
-		config = function(_, opts)
-			-- Set up mason-lspconfig with the opts
-			require("mason-lspconfig").setup(opts)
+  -- LSP Setup
+  {
+    "mason-org/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = function()
+      return {
+        -- A list of servers to automatically install if they're not already installed.
+        ensure_installed = {
+          -- Python
+          "pyright",
+          "ruff",
+          -- Latex
+          "texlab",
+          -- Markdown
+          "marksman",
+          -- C/C++
+          "clangd",
+          -- Cmake
+          "cmake",
+          -- Lua
+          "lua_ls",
+          -- Bash
+          "bashls",
+          -- Docker
+          "dockerls",
+          -- Javascript
+          "eslint",
+          -- Toml
+          "taplo",
+        },
+        -- Automatically install configured servers
+        automatic_installation = true,
+      }
+    end,
+    config = function(_, opts)
+      -- Set up mason-lspconfig with the opts
+      require("mason-lspconfig").setup(opts)
 
-			-- Configure diagnostics
-			local diag_opts = { noremap = true, silent = true }
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, diag_opts)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, diag_opts)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, diag_opts)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, diag_opts)
+      -- Configure diagnostics
+      local diag_opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, diag_opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, diag_opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, diag_opts)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, diag_opts)
 
-			vim.diagnostic.config({ virtual_text = false })
-		end,
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			{ "neovim/nvim-lspconfig", lazy = true },
-		},
-	},
+      vim.diagnostic.config({ virtual_text = false })
+    end,
+    dependencies = {
+      { "mason-org/mason.nvim",  opts = {} },
+      { "neovim/nvim-lspconfig", lazy = true },
+    },
+  },
 }
